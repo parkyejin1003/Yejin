354198f7ea60e6bb7915f26d6f4af13cf567e641
Mr.OoO <avksek56@naver.com>
2025-06-25 17:52:43 +0900

    Yejin

---
+++ 철 버튼/js/script.js
@@ -0,0 +1,15 @@
+console.clear();
+
+//it doesn't get much easier than this ;)
+const draggable = Draggable.create("#knob", {
+  type: "rotation",
+  inertia: true,
+  onDrag: function() {
+    console.log(this.rotation)
+  }
+});
+
+$("#rotation").click(function() {
+  console.log(gsap.getProperty("#knob", "rotation"), "from element");
+  console.log(Draggable.get("#knob").rotation, "from the Draggable");
+});
