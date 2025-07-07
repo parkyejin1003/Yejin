7df83c758e27317ae55dc19d1f91a3d600c26912
Mr.OoO <avksek56@naver.com>
2025-07-07 17:55:41 +0900

    Yejin

--- gentlemonster/js/script.js
+++ gentlemonster/js/script.js
@@ -1,4 +1,17 @@
 $(function(){
+
+ $(window).on("scroll", function () {
+        if ($(window).scrollTop() > 500) {
+            $("header").addClass("scrolled");
+        } else {
+            $("header").removeClass("scrolled");
+        }
+    });
+
+
+
+
+
     var swiper = new Swiper(".headerSwiper", {
       direction: "vertical",
       autoplay: {
@@ -11,14 +24,27 @@ $(function(){
       slidesPerView: 1,
       spaceBetween: 0,
       loop: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      autoplay: {
-        delay: 3000, // 3초마다 슬라이드
-        disableOnInteraction: false, // 유저 조작 후에도 계속 자동재생
-    },
+      autoplay: { delay: 5000 },
+      pagination: { el: '.swiper-pagination', clickable: true },
+
+      on: {
+        slideChange: function () {
+          // 현재 활성화된 슬라이드 요소 가져오기
+          const currentSlide = this.slides[this.activeIndex];
+          const theme = currentSlide.getAttribute('data-theme');
+
+          // 헤더에 테마에 따라 클래스 적용
+          const header = document.querySelector('header');
+          const mainSwiper = document.querySelector('.mainSwiper');
+          if (theme === 'light') {
+            header.classList.add('dark-text'); // 글자 검정색
+            mainSwiper.classList.add('dark-text'); // 글자 검정색
+          } else {
+            header.classList.remove('dark-text'); // 글자 흰색
+            mainSwiper.classList.remove('dark-text'); // 글자 흰색
+          }
+        }
+      }
     
     });
     var swiper2 = new Swiper(".modelSwiper", {
@@ -39,9 +65,52 @@ $(function(){
     });
 
     document.querySelectorAll('.modelSwiper .swiper-slide').forEach((thumb, index) => {
-  thumb.addEventListener('click', () => {
-    swiper3.slideTo(index);
+      thumb.addEventListener('click', () => {
+      swiper3.slideTo(index);
+    });
+
+
+    var banner1Swiper = new Swiper(".banner1Swiper", {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      autoplay: { delay: 2000 },
+      loop:true,
+    });
+
+    var focusSwiper = new Swiper(".focusSwiper", {
+      slidesPerView: 3,
+      centeredSlides: false,
+      slidesPerGroupSkip: 1,
+      spaceBetween: 10,
+      grabCursor: true,
+      keyboard: {
+        enabled: true,
+      },
+      breakpoints: {
+        769: {
+          slidesPerView: 3,
+          slidesPerGroup: 3,
+        },
+      },
+      scrollbar: {
+        el: ".swiper-scrollbar",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next3",
+        prevEl: ".swiper-button-prev3",
+      },
+      pagination: {
+        el: ".swiper-pagination3",
+        type: "fraction",
+      },
+    });
+
+    var bigSwiper = new Swiper(".bigSwiper", {});
   });
-});
+
+  var swiper = new Swiper(".highSwiper", {
+        slidesPerView: 4,
+        spaceBetween:0,
+    });
 
 })
